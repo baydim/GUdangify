0256e80279229a7de92e71f472ff38884a4f6c0c
YourNose <57514595+baydim@users.noreply.github.com>
2020-10-27 20:08:57 +0700

    Create satuan.dart

---
+++ lib/satuan.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+final milisec = Duration(milliseconds: 300);
+String greet;
+var putihh = Colors.white;
+var putih = TextStyle(color: Colors.white);
+final not = Align(
+    alignment: Alignment.bottomRight,
+    child: Text(
+      "Maksimal 20 karakter",
+      style: TextStyle(color: Colors.grey.shade500, fontSize: 12),
+    ));
+final nit = Align(
+    alignment: Alignment.bottomRight,
+    child: Text(
+      "@ untuk email",
+      style: TextStyle(color: Colors.grey.shade500, fontSize: 12),
+    ));
+final nut = Align(
+    alignment: Alignment.bottomRight,
+    child: Text(
+      " ",
+      style: TextStyle(color: Colors.grey.shade500, fontSize: 12),
+    ));
